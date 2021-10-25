eff3183d32971db49d8cd5f13c9dba26cfe516f1
oops-creator <still.shivam@gmail.com>
2021-10-25 23:22:31 +0530

    Add Counting Sort Algorithm

---
+++ CountingSort.py
@@ -0,0 +1,12 @@
+def countingSort(A):
+    maximum = max(A)
+    count = [0] * (maximum+1)
+    for i in A:
+        count[i] +=1
+    for i in range(1,len(count)):
+        count[i] = count[i] + count[i-1]
+    arr = [0] * len(A)
+    for i in range(len(A)-1 , -1 , -1):
+        arr[count[A[i]]-1] = A[i]
+        count[A[i]]-=1
+    return arr
